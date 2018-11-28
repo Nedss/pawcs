c3a099dd9fefa6076bdf26344984b8f96738c958
Nedss <nicolas@soirat.com>
2018-11-28 15:08:00 +0100

    Add footer and header in different php files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include "php/header.php"
+?>
+
+<div class="row">
+  <div class="col text-center">
+    <p> 
+      Bienvenue sur le site d'annonces en ligne OURS !
+      <br>
+      Vous retrouverez ici toutes les dernières annonces diffusées sur le site.
+    </p>
+  </div>
+</div>
+
+<?php
+include "php/footer.php"
+?>
+
---
+++ php/footer.php
@@ -0,0 +1,15 @@
+  <div class="row">
+    <div class="col">
+      <div class="text-center">
+        <p class="h6"><em><small><small>
+<?php
+            $DATE = date('d-m-Y');
+            echo "Rémy Costa et Nicolas Soirat. $DATE";
+          ?>
+        </small></small></em></p>
+      </div>
+    </div>
+  </div>
+  </div>
+</body>
+</html>
