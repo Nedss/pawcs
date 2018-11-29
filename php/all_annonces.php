a21b5437181ebcc0e183bd0f92caae02c9e81548
Nedss <nicolas@soirat.com>
2018-11-29 02:27:07 +0100

    sell adds complete

--- php/all_annonces.php
+++ php/all_annonces.php
@@ -11,11 +11,27 @@ include "header.php";
 <?php
 include "bdd.php";
 $maBd = connexionbd();
-$maRequete = "SELECT * FROM root.annonces;";
-$resRequete = requete($Bd, $maRequete);
+$maRequete = "SELECT * FROM nedss.annonces;";
+$resRequete = requete($maBd, $maRequete);
 
-
-
+foreach ($resRequete as $value => $line) {
+  echo "<div class='row border border-dark rounded m-2 text-center'>";
+  echo "<div class='col-4'>";
+  #echo  "<p> Ligne n°" . $value . ':' . $line . "</p>";
+  echo "<a href='" . $line['photo'] . "' >"; 
+  echo "<img src='" . $line['photo'] . "' class='img-thumbnail' alt='Photo annonce' title='Cliquez pour aggrandir' />"; 
+  echo "</a>";
+  echo "</div>";
+  echo "<div class='col-8 cell-align'>";
+  echo "<div>";
+  echo "<p>" . $line['titre'] . "</p>";
+  echo "<p>" . $line['categorie'] . "</p>";
+  echo "<p>" . $line['prix'] . " euros </p>";
+  echo "<p>" . $line['date_ajout'] . "</p>";
+  echo "</div>";
+  echo "</div>";
+  echo "</div>";
+}
 
 ?>
 
