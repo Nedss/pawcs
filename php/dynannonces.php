37f96eec3d93cf670cc29715553e1423c4baba01
Nedss <nicolas@soirat.com>
2018-11-30 03:06:43 +0100

    Add dynamic webpage for announces

---
+++ php/dynannonces.php
@@ -0,0 +1,22 @@
+<?php 
+include "header.php";
+?>
+
+<div class="row">
+  <div class="col text-center">
+    <h1> Annonces & Recherches </h1>
+  </div>
+</div>
+<div class="container" id="liste-annonces">
+</div>
+<div class="row">
+  <div class="col text-center">
+    <button class="btn btn-primary" onclick="lireAnnonces()">Afficher toutes les annonces !</button>  
+  </div>
+</div>
+
+
+<?php
+include "footer.php";
+?>
+
